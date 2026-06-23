f481ff47e92d6069df4bf6271b6730d0e77c245c
Carlos Daniel <140536944+daniusjahn@users.noreply.github.com>
2026-06-23 08:13:30 -0300

    Alterações do código (correçoes) adiçao do menu

--- Projeto_main/Locadora_filmes/src/App.java
+++ Projeto_main/Locadora_filmes/src/App.java
@@ -1,24 +1,94 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-            Locadora locadora = new Locadora();
 
-            Menu menu = new Menu(locadora);
+    public static void main(String[] args) {
 
-            Relatorio relatorio = new Relatorio();
+        Scanner scanner = new Scanner(System.in);
 
-            locadora.cadastrarFilme("Vingadores", "Ação", 1);
-            locadora.cadastrarFilme("Titanic", "Romance", 2);
+        Locadora locadora = new Locadora();
+        Menu menu = new Menu(locadora);
+        Relatorio relatorio = new Relatorio();
 
-            locadora.listarFilmes();
+        int opcao;
 
-            menu.alugarFilme(1);
+        do {
 
-            relatorio.mostrarDisponiveis(locadora);
+            System.out.println("\n===== LOCADORA DE FILMES =====");
+            System.out.println("1 - Cadastrar Filme");
+            System.out.println("2 - Listar Filmes");
+            System.out.println("3 - Alugar Filme");
+            System.out.println("4 - Devolver Filme");
+            System.out.println("5 - Mostrar Disponíveis");
+            System.out.println("6 - Mostrar Alugados");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
 
-            relatorio.mostrarAlugados(locadora);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
 
-            menu.devolverFilme(1);
+            switch (opcao) {
 
-            menu.sair();
+                case 1:
+
+                    System.out.print("Nome do filme: ");
+                    String nome = scanner.nextLine();
+
+                    System.out.print("Gênero do filme: ");
+                    String genero = scanner.nextLine();
+
+                    System.out.print("Código do filme: ");
+                    int codigo = scanner.nextInt();
+                    scanner.nextLine();
+
+                    locadora.cadastrarFilme(nome, genero, codigo);
+                    break;
+
+                case 2:
+
+                    System.out.println("\n=== LISTA DE FILMES ===");
+                    locadora.listarFilmes();
+                    break;
+
+                case 3:
+
+                    System.out.print("Digite o código do filme: ");
+                    codigo = scanner.nextInt();
+
+                    menu.alugarFilme(codigo);
+                    break;
+
+                case 4:
+
+                    System.out.print("Digite o código do filme: ");
+                    codigo = scanner.nextInt();
+
+                    menu.devolverFilme(codigo);
+                    break;
+
+                case 5:
+
+                    relatorio.mostrarDisponiveis(locadora);
+                    break;
+
+                case 6:
+
+                    relatorio.mostrarAlugados(locadora);
+                    break;
+
+                case 0:
+
+                    menu.sair();
+                    break;
+
+                default:
+
+                    System.out.println("Opção inválida!");
+
+            }
+
+        } while (opcao != 0);
+
+        scanner.close();
     }
 }
